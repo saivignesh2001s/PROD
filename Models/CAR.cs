bee37a2bc428d6361f0a404cfecbfbb846a32d29
saivignesh2001s <saivignesh2001s@gmail.com>
2022-11-09 10:20:33 +0530

    Add project files.

---
+++ Models/CAR.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PROD.Models
+{
+    public class CAR
+    {
+        public int Carid { get; set; }
+        public string Carname { get; set; }
+        public string CarType { get; set; }
+        public Nullable<int> PerDayCharge { get; set; }
+        public Nullable<int> ChargePerKm { get; set; }
+    }
+}
